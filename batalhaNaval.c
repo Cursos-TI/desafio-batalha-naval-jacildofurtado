c45daeedc2cbf25dcf9da8255b4fa1c83c454426
Francisco Jacildo Furtado da Silva <102054158+jacildofurtado@users.noreply.github.com>
2025-05-30 18:20:59 +0000

    Adicionado os navios no tabuleiro: o jogo agora valida a posição dos navios para evitar que eles ultrapassem as bordas do tabuleiro e que não se sobreponham.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,36 +1,146 @@
 #include <stdio.h>
 
 // Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
 
 int main() {
     // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    int tabuleiro [10] [10];
 
-    // Este loop aninhado preenche a matriz bidimensional com o valor 0 em todos os índices.
-    for (int i = 0; i < 10; i++) {
+    // 1. Criar o tabuleiro do jogo.
 
-        for (int j = 0; j < 10; j++) {
+    int tabuleiro [10] [10]; // Matriz para simular um tabuleiro 10 x 10.
+
+    // Este loop aninhado preenche a matriz do tabuleiro com o valor 0 em todos os índices representando água.
+    for (int i = 0; i < 10; i++) { // Conta as linhas.
+
+        for (int j = 0; j < 10; j++) { // Conta as colunas.
             tabuleiro[i][j] = 0;
         }
+
     }
 
+    // 2. Posicionamento dos navios.
 
+    // Definição de vetores com comprimento 3 para representar os navios.
+    int navio1[3] = {3, 3, 3};
+    int navio2[3] = {3, 3, 3};
 
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    // Coordenadas navio 1 - Horizontal.
+    int linha1 = 9;
+    int colunaInicial1 = 5;
+    int podePosicionar1 = 1;
 
-    printf("***** TABULEIRO BATALHA NAVAL *****\n");
-    for (int k = 0; k < 10; k++) { // Esse loop controla a linha
-        
-        for(int l = 0; l < 10; l++) { // Esse loop controla a coluna
-            printf("%d ", tabuleiro[k][l]);
+    // Coordenadas do navio 2 - Vertical.
+    int coluna2 = 9;
+    int linhaInicial2 = 7;
+    int podePosicionar2 = 1;
+
+    // Verifica se o navio 1 cabe no tabuleiro.
+    if (colunaInicial1 + 3 <= 10 && linha1 <= 10) { // Verifica se o navio cabe na horizontal sem ultrapassar as bordas do tabuleiro.
+
+        for (int i = 0; i < 3; i++) { // Verifica se as posições onde o navio será colocado já estão ocupadas.
+
+            if(tabuleiro[linha1][colunaInicial1 + i] != 0){
+                podePosicionar1 = 0;
+                break;
+            }
+
         }
 
-        printf("\n"); // Quebra uma linha no fim de cada coluna.
+    } else {
+
+        podePosicionar1 = 0;
+
     }
+
+    // Posicionamento do navio 1 na horizontal se ele puder ser posicionado.
+    if (podePosicionar1 == 1) {
+
+        for(int i = 0; i < 3; i++) {
+
+            tabuleiro[linha1][colunaInicial1 + i] = navio1[i]; // A linha é fixa e o loop percorre a coluna substituindo o valor no tabuleiro pelo navio.
+
+        }
+
+    } else {
+
+        printf("Erro: navio 1 fora dos limites ou sobreposição!\n");
+
+    }
+
+    // Verifica se o navio 2 cabe no tabuleiro.
+    if (linhaInicial2 + 3 <= 10 && coluna2 <= 10) { // Verifica se o navio cabe na vertical sem ultrapassar as bordas do tabuleiro.
+
+        for (int i = 0; i < 3; i++) { // Verifica se as posições onde o navio será colocado já estão ocupadas.
+
+            if (tabuleiro[linhaInicial2 + i][coluna2] != 0) {
+                podePosicionar2 = 0;
+                break;
+            }
+
+        }
+
+    } else {
+
+        podePosicionar2 = 0;
+
+    }
+
+    // Posionamento do navio 2 se ele puder ser posicionado
+    if (podePosicionar2 == 1) {
+
+        for (int i = 0; i < 3; i++) {
+            tabuleiro[linhaInicial2 + i][coluna2] = navio2[i]; // A coluna é fixa e o loop percorre a linha substituindo o valor no tabuleiro pelo navio.
+        }
+
+    } else {
+
+        printf("Erro: navio 2 fora dos limites ou sobreposição!\n");
+
+    }
+
+
+    // 3. Exibição do tabuleiro do jogo
+
+    if (podePosicionar1 == 1 && podePosicionar2 == 1) {
+
+        printf("***** TABULEIRO BATALHA NAVAL *****\n");
+
+        printf("\n"); // Deixa uma linha vazia.
+
+        printf("    "); // Adiciona um espaçamento para alinhar a linha das letras com o tabuleiro.
+
+        // Coloca uma linha de letras no tabuleiro.
+        for (char letra = 'A'; letra <= 'J'; letra++) {
+
+            printf("%c ", letra);
+
+        }
+
+        printf("\n"); // Quebra a linha das letras.
+        
+
+        // Coloca uma coluna numerada.
+        for (int i = 0; i < 10; i++) {
+
+            printf("%2d  ", i + 1);
+
+            for (int j = 0; j < 10; j++) { // Esse loop controla a linha
+                printf("%d ", tabuleiro[i][j]);  
+            }
+
+            printf("\n"); // Quebra uma linha no fim de cada coluna.
+            
+        }
+
+    } else {
+
+        printf("Erro: Verifique o posicionamento dos navios!\n");
+
+    }
+
+    
+
+
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
